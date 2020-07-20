c856e77905e7b8e709507d1081467592ea79f5fd
Peter Peterson <peter.peterson.iv@gmail.com>
2020-07-20 10:14:28 -0700

    added .org files too

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,7 +51,7 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 )
 
 debug "Enumerating contents of $1"
-for file in $(find $1 -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
+for file in $(find $1 -maxdepth 1 -type f -name '*{.md,.org}' -execdir basename '{}' ';'); do
     debug "Copying $file"
     cp "$1/$file" "$tmp_dir"
 done
